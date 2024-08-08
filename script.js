e687f68497c2e2694d9281cd48fbe90ca06b2b4e
iyang2006 <yang2006@purdue.edu>
2024-08-08 17:16:26 -0600

    Successfully created slideshow

---
+++ script.js
@@ -0,0 +1,15 @@
+let root = document.documentElement;
+let slideshowPics = document.querySelectorAll("#slideshow img");
+let slideshowWidth = 0;
+let slideshowSpeed = 30;
+
+// Get the total slideshow width
+for (let i = 0; i < slideshowPics.length; i++) {
+    slideshowWidth += slideshowPics[i].clientWidth;
+}
+
+// Set the slider width and the appropriate speed
+root.style.setProperty("--sliderWidth", -slideshowWidth + "px");
+root.style.setProperty("--sliderTime", slideshowWidth / slideshowSpeed + "s");
+
+console.log(slideshowWidth / slideshowSpeed);
