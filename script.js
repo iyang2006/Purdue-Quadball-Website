56155ecfd08546dee1f2d9f8641792af3eec4dfc
iyang2006 <yang2006@purdue.edu>
2024-09-13 21:13:46 -0400

    a

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let root = document.documentElement;
-let slideshowPics = document.querySelectorAll("#slideshow img");
+let slideshowPics = document.querySelectorAll("#slideshow #slideImg img");
 let slideshowWidth = 0;
 let slideshowSpeed = 30;
 
@@ -12,19 +12,4 @@ for (let i = 0; i < slideshowPics.length; i++) {
 root.style.setProperty("--sliderWidth", -slideshowWidth + "px");
 root.style.setProperty("--sliderTime", slideshowWidth / slideshowSpeed + "s");
 
-// Reorders the slider every 3 seconds
-setInterval(reorderSlider, 3000);
-
-// Reorders the slider so that once one picture is scrolled past the viewport,
-// that picture can be reordered to the back of the flexbox
-function reorderSlider() {
-
-}
-
-
-//setTimeout(testing, 3000);
-function testing() {
-    slideshowPics[0].style.order = 1;
-    //document.querySelector("#slideshow").style
-}
 console.log(slideshowWidth / slideshowSpeed);
